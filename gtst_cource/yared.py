798c14f888ecd943f63d112d64d1e64b037ed436
yata13 <122173310+yata13@users.noreply.github.com>
2023-04-01 07:26:03 +0300

    day6_py.md
    day6_py.md

---
+++ gtst_cource/yared.py
@@ -0,0 +1,9 @@
+gtst= "day7"
+print ("hello today is our " ,gtst, " course!")
+y = [0,2,4]
+print('the 1st even number is ',y[0])
+print('the 2nd even number is ',y[1])
+print('the 3rd even number is ',y[2])
+fruits = {'apple': '10', 'banana': '15', 'pineapple': '20'}
+choise = input ("choise your faverite fruits \n")
+print ("the value of the" ,choise, "is" ,(fruits[choise]))
